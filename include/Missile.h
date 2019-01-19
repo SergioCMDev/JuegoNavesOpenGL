9092da33969a9880f2058d5bddf8be8d7e0a297f
Sergio Cristauro Manzano <sergio_c_m@hotmail.com>
2019-01-19 12:46:39 +0100

    Rotación misiles para enemigos OK

--- include/Missile.h
+++ include/Missile.h
@@ -13,12 +13,13 @@ public:
 	//float GetVelocity();
 	void Mover(const Movement movement, const float deltaTime);
 
-	void Rotate();
+	void Rotate(glm::mat4 &_modelMatrix);
 	Missile(Shader & shader, glm::vec3 position, GameObject parent);
 	~Missile();
 	void Render(glm::mat4 &projection, glm::mat4 &view);
 private:
 	const char* pathToModel = "../assets/obj/Missile/asca.obj";
 	glm::mat4 _modelMatrix;
+	GameObject* _parent;
 };
 #endif
--- src/Missile.cpp
+++ src/Missile.cpp
@@ -5,7 +5,6 @@ Missile::Missile() {
 }
 Missile::Missile(Shader & shader, glm::vec3 position, GameObject parent)
 {
-	//position = vec3(0.0f, 0.0f, 0.0f);
 	_model = Model(pathToModel);
 	_shader = shader;
 	SetPosition(position);
@@ -13,9 +12,7 @@ Missile::Missile(Shader & shader, glm::vec3 position, GameObject parent)
 	_type = Constants::TIPO_MISIL;
 	//_modelMatrix = mat4(1.0f);
 	_scale = vec3(0.4f);
-	if (parent._type == Constants::TIPO_PLAYER) {
-		Rotate();
-	}
+	_parent = &parent;
 }
 
 Missile::~Missile() {
@@ -30,16 +27,17 @@ void Missile::Render(glm::mat4 &projection, glm::mat4 &view)
 	_modelMatrix = glm::translate(_modelMatrix, _position);
 
 	_modelMatrix = glm::scale(_modelMatrix, _scale);
-
+	if (_parent->_type == Constants::TIPO_ENEMIGO) {
+		Rotate(_modelMatrix);
+	}
 	_shader.Set("projection", projection);
 	_shader.Set("view", view);
 	_shader.Set("model", _modelMatrix);
 	_model.Draw(_shader);
 }
 
-void Missile::Rotate() {
-	_modelMatrix = glm::rotate(_modelMatrix, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-	_shader.Set("model", _modelMatrix);
+void Missile::Rotate(glm::mat4 &_modelMatrix) {
+	_modelMatrix = glm::rotate(_modelMatrix, glm::radians(180.0f), glm::vec3(1.0f, 1.0f, 0.0f));
 
 }
 
